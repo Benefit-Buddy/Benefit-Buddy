38f3e2a8177b97b8f687345dc9af8e5d7ad20b73
Benefit-Buddy <167063962+Benefit-Buddy@users.noreply.github.com>
2024-04-16 14:57:11 +0530

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css';
-  import {BsMask} from "react-icons/bs";
+  // import {BsMask} from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
